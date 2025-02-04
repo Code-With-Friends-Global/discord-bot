0fd4b8bb64b226383f8034d5cede73d86c59b1a4
Paul Pham <ppham@Pauls-MBP.attlocal.net>
2025-02-03 21:38:54 -0600

    Add setup instructions, and send first successful embed as reply to /server

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,5 +1,29 @@
 import { SlashCommandBuilder } from 'discord.js';
 
+import { EmbedBuilder } from 'discord.js';
+
+const exampleEmbed = new EmbedBuilder()
+  .setColor(0x0099FF)
+  .setTitle('Some title')
+  .setURL('https://discord.js.org/')
+  .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+  .setDescription('Some description here')
+  .setThumbnail('https://tenor.com/view/matt-frewer-1980s-1985-computer-animation-comedians-gif-16976473')
+  // Add fields in horizontal rows
+  .addFields(
+	{ name: 'Regular field title', value: 'Some value here' },
+	{ name: '\u200B', value: '\u200B' },
+	{ name: 'First inline field title', value: 'Some first field value', inline: true },
+	{ name: 'Second inline field title', value: 'Some value here', inline: true },
+  )
+  .addFields(
+	{ name: 'Inline field title', value: 'Some value', inline: true }
+  )
+  .setImage('https://i.imgur.com/AfFp7pu.png')
+  .setTimestamp()
+  .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png'})
+;
+
 export default {
 	data: (
 		new SlashCommandBuilder()
@@ -9,8 +33,13 @@ export default {
 
 	async execute(interaction) {
 		await interaction.reply(
+			{ embeds: [exampleEmbed] }
+		);
+		/*
+		await interaction.reply(
 			`This server is ${interaction.guild.name}`
 			+ ` and has ${interaction.guild.memberCount} members.`
 		);
+		*/
 	},
 };
